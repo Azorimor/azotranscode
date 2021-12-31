f0a1af0213ccf88136e83daf5f26d47ea2844dcc
Azorimor <azorimor@gmail.com>
2021-12-31 18:57:14 +0100

    Updated footer to contain project name
    Signed-off-by: Azorimor <azorimor@gmail.com>

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -98,7 +98,7 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} Azotranscode - Built with Docusaurus.`,
       },
       prism: {
         theme: lightCodeTheme,
