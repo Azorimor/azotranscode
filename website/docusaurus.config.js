c6590a19517de5dd936d1ded89df795a31a2aae4
Azorimor <azorimor@gmail.com>
2021-12-31 18:45:56 +0100

    Added github actions
    Signed-off-by: Azorimor <azorimor@gmail.com>

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -15,7 +15,7 @@ const config = {
   favicon: 'img/favicon.ico',
   organizationName: 'azorimor', // Usually your GitHub org/user name.
   projectName: 'azotranscode', // Usually your repo name.
-
+  trailingSlash: false,
   presets: [
     [
       'classic',
