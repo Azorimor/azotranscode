d17923d7d9ff3e1b86cf8065fdf4822d83814138
Azorimor <azorimor@gmail.com>
2022-01-01 16:16:51 +0100

    restructured documentation
    Signed-off-by: Azorimor <azorimor@gmail.com>

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -51,7 +51,7 @@ const config = {
         items: [
           {
             type: 'doc',
-            docId: 'intro',
+            docId: 'getting-started/getting-started-intro',
             position: 'left',
             label: 'Docs',
           },
